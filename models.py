f162059614083959509848fe7ecdaae29d473b96
Andrew Omori <andrewomori7@gmail.com>
2025-08-28 15:45:58 +0300

    fix dependancies

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from datetime import date
 
